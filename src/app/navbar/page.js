dd44ca7838cc6d3f2fdfe31726de0c9071e4a0da
Hannah <hannah1155@naver.com>
2024-01-08 16:12:20 +0900

    style.jsx 형식 예시

--- src/app/navbar/page.js
+++ src/app/navbar/page.js
@@ -2,18 +2,28 @@
 
 import Link from 'next/link';
 import { usePathname  } from 'next/navigation';
-import styles from "./Navbar.module.css";
 
 export default function NavBar() {
     const pathname  = usePathname();
     return (
         <nav>
             <Link href="/" legacyBehavior>
-                <a className={[styles.link, pathname === '/' ? styles.active : ''].join(" ")}>Home</a>
+                <a className={pathname === "/" ? "active" : ""}>Home</a>
             </Link>
             <Link href="/about" legacyBehavior>
-                <a className={[styles.link, pathname === '/about' ? styles.active : ''].join(" ")}>about</a>
+                <a className={pathname === "/about" ? "active" : ""}>About</a>
             </Link>
+            <style jsx>{`
+                nav {
+                    background-color: tomato;
+                }
+                a {
+                    text-decoration:none;
+                }
+                .active {
+                    color: yellow;
+                }
+            `}</style>
         </nav>
     );
 }
